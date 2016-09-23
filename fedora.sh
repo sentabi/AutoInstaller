9b9da370c1d20990e0440ea75dfb931c92f83d1b
Tommy A. Surbakti <tommy@surbakti.net>
2016-09-23 23:24:22 +0700

    hapus log

--- fedora.sh
+++ fedora.sh
@@ -4,84 +4,81 @@
 ## jangan asal di jalankan, liat dulu scriptna untuk menghindari hal-hal yang tidak 
 ## diinginkan
 
-LOG=/tmp/fedora.log
-
-# hapus log yang sudah ada
-rm -f $LOG
-touch $LOG
-
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
-# Hapus aplikasi yang ngga perlu 
-dnf remove transmission claws-mail-* midori pidgin -y | tee -a $LOG
-dnf remove abrt-* -y | tee -a $LOG
+# Zona waktu WIB
+rm -f /etc/localtime
+cp /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
-# Update Repo dan Upgrade
-dnf clean all | tee -a $LOG
-dnf update -y | tee -a $LOG
-dnf upgrade -y | tee -a $LOG
+# Hapus aplikasi yang ngga perlu 
+dnf remove transmission claws-mail* midori pidgin -y
+dnf remove abrt-* -y
 
 # RPM FUSION
-dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y | tee -a $LOG
+dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
+# Update Repo dan Upgrade
+dnf clean all
+dnf update -y
+dnf upgrade -y
 
 # install aplikasi
-dnf install gimp inkscape vnstat terminator git puddletag pavucontrol tigervnc shotwell htop nano wireshark nmap uget rfkill remmina remmina-plugins* openvpn -y | tee -a $LOG
+dnf install wget gimp inkscape vnstat terminator git puddletag pavucontrol tigervnc shotwell nano wireshark nmap uget rfkill remmina remmina-plugins* openvpn -y
 
 # Torrent Client 
-dnf install deluge -y | tee -a $LOG
+dnf install deluge -y
 
 # Password Manager 
-dnf install keepassx -y | tee -a $LOG
+dnf install keepassx -y
 
 # ownCloud Client
-dnf install owncloud-client -y | tee -a $LOG
+dnf install owncloud-client -y
 
 # install sublime 3
-wget https://download.sublimetext.com/sublime_text_3_build_3114_x64.tar.bz2 | tee -a $LOG 
-tar jxvf sublime_text_3_build_*.tar.bz2 | tee -a $LOG
+wget https://download.sublimetext.com/sublime_text_3_build_3124_x64.tar.bz2
+tar jxvf sublime_text_3_build_*.tar.bz2
 mv sublime_text_3 /opt
 ln -s /opt/sublime_text_3/sublime_text /usr/bin/sublime
 
 # XFCE
-dnf install xfce4-mixer xfce4-volumed bluebird-gtk3-theme bluebird-gtk2-theme bluebird-xfwm4-theme y | tee -a $LOG
+dnf install xfce4-pulseaudio-plugin bluebird-gtk3-theme bluebird-gtk2-theme bluebird-xfwm4-theme y
 
 # codec multimedia
-dnf install  gstreamer-plugins-* gstreamer1-* ffmpeg youtube-dl -y | tee -a $LOG
+dnf install  gstreamer-plugins-* gstreamer1-* ffmpeg youtube-dl -y
 # Multimedia Player
-dnf install vlc smplayer clementine -y | tee -a $LOG
+dnf install vlc smplayer clementine -y
 
 # VirtualBox
-wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo -O /etc/yum.repos.d/virtualbox.repo | tee -a $LOG
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | rpm --import - | tee -a $LOG
-dnf install gcc make dkms kernel-devel-$(uname -r) kernel-headers VirtualBox-5.1 -y | tee -a $LOG
+wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo -O /etc/yum.repos.d/virtualbox.repo
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | rpm --import -
+dnf install gcc make dkms kernel-devel-$(uname -r) kernel-headers VirtualBox-5.1 -y
 
 # ekstrator 
-dnf install file-roller-nautilus file-roller unzip unrar p7zip unrar -y | tee -a $LOG
+dnf install file-roller-nautilus file-roller unzip unrar p7zip unrar -y
 
 # Mount Android/Samba
-dnf install libmtp-devel libmtp gvfs-mtp simple-mtpfs libusb gvfs-client gvfs-smb gvfs-fuse gigolo -y | tee -a $LOG
+dnf install libmtp-devel libmtp gvfs-mtp simple-mtpfs libusb gvfs-client gvfs-smb gvfs-fuse gigolo -y
 
 # Browser dan Email Client
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm | tee -a $LOG
-dnf install google-chrome-stable_current_x86_64.rpm -y | tee -a $LOG
-dnf install thunderbird firefox -y | tee -a $LOG
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+dnf install google-chrome-stable_current_x86_64.rpm -y
+dnf install thunderbird firefox -y
 
 # network 
-dnf install mtr -y | tee -a $LOG
+dnf install rsync htop rsnapshot vnstat mtr iperf curl -y
 
 # LibreOffice 
-dnf install libreoffice -y | tee -a $LOG
+dnf install libreoffice -y
  
 ## LAMP untu Web Development
-dnf install httpd mariadb mariadb-server php php-pdo phpMyAdmin php-cli php-mysqlnd php-mcrypt php-xml -y | tee -a $LOG
+dnf install httpd mariadb mariadb-server php php-pdo phpMyAdmin php-cli php-mysqlnd php-mcrypt php-xml -y
 
 ### Install Composer 
-curl -sS https://getcomposer.org/installer | php | tee -a $LOG
-mv composer.phar /usr/bin/composer | tee -a $LOG
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/bin/composer
 
 ## Font Rendering
 echo '<?xml version="1.0"?>
@@ -92,9 +89,9 @@ echo '<?xml version="1.0"?>
 <bool>true</bool>
 </edit>
 </match>
-</fontconfig>' > /etc/fonts/conf.d/99-autohinter-only.conf | tee -a $LOG
-ln -s /etc/fonts/conf.avail/10-autohint.conf /etc/fonts/conf.d/ | tee -a $LOG
-ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/ | tee -a $LOG
+</fontconfig>' > /etc/fonts/conf.d/99-autohinter-only.conf
+ln -s /etc/fonts/conf.avail/10-autohint.conf /etc/fonts/conf.d/
+ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
  
 echo '<?xml version="1.0"?>
 <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
@@ -124,20 +121,20 @@ echo '<?xml version="1.0"?>
    <bool>true</bool>
   </edit>
  </match>
-</fontconfig>' > /home/$USER/.fonts.conf | tee -a $LOG
+</fontconfig>' > /home/$USER/.fonts.conf
 
 ## Generate SSH Key
-ssh-keygen -b 4096 
+# ssh-keygen -b 4096 
 
 # Font 
-dnf install freetype-freeworld -y | tee -a $LOG
-dnf install https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm -y | tee -a $LOG
-wget http://font.ubuntu.com/download/ubuntu-font-family-0.83.zip | tee -a $LOG
-unzip ubuntu-font-family-0.83.zip | tee -a $LOG
-mv ubuntu-font-family-0.83 /usr/share/fonts/ | tee -a $LOG
-wget https://github.com/downloads/adobe-fonts/source-code-pro/SourceCodePro_FontsOnly-1.013.zip | tee -a $LOG
-unzip SourceCodePro_FontsOnly-1.013.zip | tee -a $LOG
-mv SourceCodePro_FontsOnly-1.013 /usr/share/fonts/ | tee -a $LOG
+dnf install freetype-freeworld -y
+dnf install https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm -y
+wget http://font.ubuntu.com/download/ubuntu-font-family-0.83.zip
+unzip ubuntu-font-family-0.83.zip
+mv ubuntu-font-family-0.83 /usr/share/fonts/
+wget https://github.com/downloads/adobe-fonts/source-code-pro/SourceCodePro_FontsOnly-1.013.zip
+unzip SourceCodePro_FontsOnly-1.013.zip
+mv SourceCodePro_FontsOnly-1.013 /usr/share/fonts/
 
 # Tweak XFCE 
 xfconf-query -c xfce4-panel -p /plugins/plugin-1/show-button-title -s "false"
@@ -145,8 +142,15 @@ xfconf-query -c xfce4-panel -p /plugins/plugin-1/button-icon -s "ibus-hangul"
 xfconf-query -c xfwm4 -p /general/theme -s "Bluebird"
 xfconf-query -c xsettings -p /Net/ThemeName -s "Glossy"
 
-dnf remove xfce4-pulseaudio-plugin
-dnf install xfce4-volumed
-
 # Disable Selinux. Enable setelah semua di testing ;)
 sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/selinux/config 
+
+# Mengamankan /tmp
+cd ~
+rm -rf /tmp
+mkdir /tmp
+mount -t tmpfs -o rw,noexec,nosuid tmpfs /tmp
+chmod 1777 /tmp
+echo "tmpfs   /tmp    tmpfs   rw,noexec,nosuid        0       0" >> /etc/fstab
+rm -rf /var/tmp
+ln -s /tmp /var/tmp
