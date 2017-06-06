b093c0af0b83784970117aa05e66f9e37fa232ed
Tommy A. Surbakti <tommy@surbakti.net>
2017-06-06 22:12:50 +0700

    updated

--- README.md
+++ README.md
@@ -29,3 +29,22 @@ wget --no-check-certificate https://git.io/voUvT -O fedora.sh; chmod +x fedora.s
 Distro Linux yang sudah di coba
 - Debian 7,8 64 Bit
 - Fedora 24,25 64 Bit
+
+### Serper Online 
+```
+PS1="\[\e[1;30m\][\[\e[1;33m\]\u@\H\[\e[1;30m\]\[\e[0;32m\]\[\e[1;30m\]] \[\e[1;37m\]\w\[\e[0;37m\] \n\$ "
+```
+### Serper Offline/Serper Lokal
+```
+PS1="\[\e[1;30m\][\[\e[1;94m\]\u@\H\[\e[1;30m\]\[\e[0;32m\]\[\e[1;30m\]] \[\e[1;37m\]\w\[\e[0;37m\] \n\$ "
+```
+### Komputer/PC
+```
+PS1="\[\e[1;30m\][\[\e[1;31m\]\u@\H\[\e[1;30m\]\[\e[0;32m\]\[\e[1;30m\]] \[\e[1;37m\]\w\[\e[0;37m\] \n\$ "
+```
+
+## Carana 
+simpan salah sada baris sidatas kubas ~/.bashrc
+```
+echo 'PS1="\[\e[1;30m\][\[\e[1;33m\]\u@\H\[\e[1;30m\]\[\e[0;32m\]\[\e[1;30m\]] \[\e[1;37m\]\w\[\e[0;37m\] \n\$ "' >> ~/.bashrc
+```
---
+++ arm.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+# Debian/Ubuntu ARM
+
+if [ "$(id -u)" != "0" ]; then
+   echo "Harus dijalankan sebagai root" 1>&2
+   exit 1
+fi
+
+if [[ ! -e /etc/debian_version ]]; then
+echo "Hanya bisa dijalankan di Debian"
+exit
+fi
+
+# update repository dan sistem
+apt-get clean all
+apt-get update
+apt-get upgrade -y
+
+# update timezone  Jakarta
+echo "Asia/Jakarta" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+
+# tambah US UTF8 sama Indonesia
+# Biar format tanggal bisa menggunakan tanggal Indonesia
+echo '
+en_US.UTF-8 UTF-8
+id_ID ISO-8859-1
+id_ID.UTF-8 UTF-8
+' > /etc/locale.gen
+locale-gen en_US.UTF-8
+
+# install ca-certificates biar wget ga protest ERROR: The certificate of xxxxxx
+apt-get install wget bsdutils bash-completion nano dialog curl ca-certificates -y
+
+# nano Syntax highlight
+echo '
+set autoindent
+syntax "comments" ".*"
+color blue "^#.*"
+set morespace
+include /usr/share/nano/nginx.nanorc
+' >> ~/.nanorc
+wget https://raw.githubusercontent.com/scopatz/nanorc/master/nginx.nanorc -O /usr/share/nano/nginx.nanorc
+find /usr/share/nano/ -iname "*.nanorc" -exec echo include {} \; >> ~/.nanorc
+
+# konfigurasi ulang OpenSSH server'
+dpkg-reconfigure openssh-server
+
+# PS1
+echo 'PS1="\[\e[1;30m\][\[\e[1;33m\]\u@\H\[\e[1;30m\]\[\e[0;32m\]\[\e[1;30m\]] \[\e[1;37m\]\w\[\e[0;37m\] \n\$ "' >> ~/.bashrc
+source ~/.bashrc
+
+# SSH
+echo "UseDNS no" >> /etc/ssh/sshd_config
+
+# Network Tools
+apt-get install rsync htop rsnapshot vnstat mtr iperf curl unzip openvpn whois dnsutils strace ltrace zip -y
+
+# Generate SSH Key
+ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa -q
+
+# Add public_key
+wget --no-check-certificate https://raw.githubusercontent.com/sentabi/AutoInstaller/master/id_rsa.pub -O ~/.ssh/authorized_keys
