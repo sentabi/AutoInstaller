1ad0a9118b7ca53bd2fcc3699115f262e2a04a46
Tommy A. Surbakti <tommy@surbakti.net>
2017-10-10 11:18:58 +0700

    repo mariadb

--- centos.sh
+++ centos.sh
@@ -60,6 +60,13 @@ yum install git -y
 yum install fail2ban sendmail -y
 
 # MariaDB
+echo '
+[mariadb]
+name = MariaDB
+baseurl = http://yum.mariadb.org/10.1/centos7-amd64
+gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+gpgcheck=1
+' > /etc/yum.repos.d/MariaDB.repo
 yum install mariadb-server mariadb -y
 
 # PHP71
